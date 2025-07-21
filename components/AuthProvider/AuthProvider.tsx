f2d8a8b3ba18c63e204d4c3422b8760fdcfc5925
Татьяна Татьяна <taty.bystrova@gail.com>
2025-07-21 20:39:44 +0300

    change AuthProvider.tsx

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -14,15 +14,16 @@ const AuthProvider = ({ children }: Props) => {
         (state) => state.clearIsAuthenticated
     );
 
-  useEffect(() => {
+    useEffect(() => {
     const fetchUser = async () => {
-        const isAuthenticated = await checkSession();
-            if (isAuthenticated) {
-                const user = await getMe();
-            if (user) setUser(user);
-            } else {
-                clearIsAuthenticated();
-            }
+      checkSession()
+        .then(async () => {
+          const user = await getMe();
+          if (user) setUser(user);
+        })
+        .catch(() => {
+         clearIsAuthenticated();
+        });
     };
     fetchUser();
   }, [setUser, clearIsAuthenticated]);
